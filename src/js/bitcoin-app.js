3f94f1717af1cfd5a544c8dba8dfa6ad3baa0c1c
Fredrik Norrman <fredden87@gmail.com>
2020-01-14 14:47:09 +0100

    base functions for bitcoin-app

--- src/js/bitcoin-app.js
+++ src/js/bitcoin-app.js
@@ -3,6 +3,8 @@ template.innerHTML = /* html */`
 <style>
 </style>
 <div id="bitcoin">
+<ul id="messages">
+</ul>
 </div>
 `
 
@@ -11,6 +13,8 @@ export default class BitcoinApp extends window.HTMLElement {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this._bitcoinMessages = this.shadowRoot.querySelector('#messages')
+    this._socket = new window.WebSocket('wss://ws.blockchain.info/inv')
   }
 
   static get observedAttributes () {
@@ -22,11 +26,33 @@ export default class BitcoinApp extends window.HTMLElement {
   }
 
   connectedCallback () {
+    this._socket.addEventListener('open', this._connected)
+    this._socket.addEventListener('message', (event) => {
+      this._receive(event)
+    })
+  }
 
+  disconnectedCallback () {
+    this._socket.close()
   }
 
   _updateRendering () {
 
+  }
+
+  _connected (event) {
+    console.log('connected')
+  }
+
+  _receive (event) {
+    const data = JSON.parse(event.data)
+    if (this._bitcoinMessages.childNodes.length === 50) {
+      this._bitcoinMessages.removeChild(this._bitcoinMessages.childNodes[1])
+    }
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(`${data}`))
+    this._chatMessages.appendChild(li)
+    this._chat.scrollTop = this._chat.scrollHeight
   }
 }
 window.customElements.define('bitcoin-app', BitcoinApp)
